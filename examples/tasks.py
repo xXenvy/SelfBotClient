c599ffc0491eff53a4c5a8e91560c229ca3ba2f9
Xenvy <111158232+xXenvy@users.noreply.github.com>
2023-07-09 02:54:55 +0200

    Update tasks.py

--- examples/tasks.py
+++ examples/tasks.py
@@ -20,8 +20,7 @@ if __name__ == "__main__":
     # add Tasks to the main Thread and then run it
     func = spam_messages
 
-    client.tasks.add_task(func=func(channel_id=123, message_content="test", times=100), name="spam_1")
-    client.tasks.add_task(func=func(channel_id=123, message_content="test", times=100), name="spam_2")
-    client.tasks.add_task(func=func(channel_id=123, message_content="test", times=100), name="spam_2")
+    for x in range(3):
+        client.tasks.add_task(func=func(channel_id=123, message_content="test", times=100), name=f"spam_{x}")
 
     client.tasks.run() # At this point, we run 3 tasks simultaneously. Which allows us to send 3 messages at once.
